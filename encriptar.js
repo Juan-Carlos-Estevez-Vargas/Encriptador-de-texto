d0e1940f0b6bd314cdf9111ebd86b44b00b2b637
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2022-10-04 14:51:17 -0500

    Optimizaciones leves en el código

--- encriptar.js
+++ encriptar.js
@@ -1,33 +1,41 @@
 function encriptar() {
   let texto = document.getElementById("texto").value;
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+  let imagen = document.getElementById("muñeco");
+
   let textoCifrado = texto
     .replace(/e/gi, "enter")
     .replace(/i/gi, "imes")
     .replace(/a/gi, "ai")
     .replace(/o/gi, "ober")
     .replace(/u/gi, "ufat");
-  if (document.getElementById("texto").value.length != 0) {
-    document.getElementById("texto").value = textoCifrado;
-    document.getElementById("titulo-mensaje").textContent =
-      "Texto encriptado con éxito";
-    document.getElementById("parrafo").textContent = "";
-    document.getElementById("muñeco").src = "/img/encriptado.jpg";
+
+  if (texto.length != 0) {
+    texto = textoCifrado;
+    tituloMensaje.textContent = "Texto encriptado con éxito";
+    parrafo.textContent = "";
+    imagen.src = "/img/encriptado.jpg";
   }
 }
 
 function desencriptar() {
   let texto = document.getElementById("texto").value;
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+  let imagen = document.getElementById("muñeco");
+
   let textoCifrado = texto
     .replace(/enter/gi, "e")
     .replace(/imes/gi, "i")
     .replace(/ai/gi, "a")
     .replace(/ober/gi, "o")
     .replace(/ufat/gi, "u");
-  if (document.getElementById("texto").value.length != 0) {
-    document.getElementById("texto").value = textoCifrado;
-    document.getElementById("titulo-mensaje").textContent =
-      "Texto desencriptado con éxito";
-    document.getElementById("parrafo").textContent = "";
-    document.getElementById("muñeco").src = "/img/desencriptado.jpg";
+
+  if (texto.length != 0) {
+    texto = textoCifrado;
+    tituloMensaje.textContent = "Texto desencriptado con éxito";
+    parrafo.textContent = "";
+    imagen.src = "/img/desencriptado.jpg";
   }
 }
